ea4cffee6e4b774aa2aa09a834b7061e614b129f
hyunjung <hyunj8764@gmail.com>
2020-07-28 21:20:11 +0900

    mypage_review.xml(작성한코멘트목록 페이지) 추가

---
+++ app/src/main/java/com/example/reviews/MyReviewActivity.java
@@ -0,0 +1,15 @@
+package com.example.reviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage_review);
+
+    }
+}
