865987ceec24f3d71140685c6660a50735d8c430
hyunjung <hyunj8764@gmail.com>
2020-08-05 04:03:06 +0900

    메인페이지,리뷰상세페이지 디자인 일부 수정

--- app/src/main/java/com/example/reviews/MainActivity.java
+++ app/src/main/java/com/example/reviews/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.reviews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TabHost;
 import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
 
 // 메인페이지 java 파일
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +24,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost1 = (TabHost)findViewById(R.id.tabHost1);
+        tabHost1.setup();
+
+        // 첫번째 탭
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("movie_rating_ranking");
+        ts1.setContent(R.id.content1);
+        ts1.setIndicator("영화평점순위");
+        tabHost1.addTab(ts1);
+
+        // 두번째 탭
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("movie_rating_ranking");
+        ts2.setContent(R.id.content2);
+        ts2.setIndicator("영화추천순위");
+        tabHost1.addTab(ts2);
+
+        // 세번째 탭
+        TabHost.TabSpec ts3 = tabHost1.newTabSpec("movie_rating_ranking");
+        ts3.setContent(R.id.content3);
+        ts3.setIndicator("영화신작");
+        tabHost1.addTab(ts3);
 
         // 하단바 underbar_home 버튼 등록 및 리스너 구현
         btnHome = (ImageButton)findViewById(R.id.underbar_home);
