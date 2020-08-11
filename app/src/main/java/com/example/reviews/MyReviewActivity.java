ac9c42f164e7fd4d1f5a278372d0761b405e956a
hyunjung <hyunj8764@gmail.com>
2020-08-11 17:45:22 +0900

    mypage_review_detail.xml 수정

--- app/src/main/java/com/example/reviews/MyReviewActivity.java
+++ app/src/main/java/com/example/reviews/MyReviewActivity.java
@@ -6,9 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 // 마이페이지 -> 작성한코멘트 목록 페이지 java 파일
 public class MyReviewActivity extends AppCompatActivity {
+
+    // 작성한코멘트의 리뷰글
+    LinearLayout myReview1;
+
+    // 영화 포스터
+    ImageView poster;
 
     // 하단바 버튼
     ImageButton btnHome;
@@ -20,6 +31,24 @@ public class MyReviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage_review);
 
+        // 영화 포스터 클릭시 영화정보페이지로 넘어감
+        poster = (ImageView)findViewById(R.id.movie_poster);
+        poster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MovieInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+        // 리뷰 클릭시 리뷰 상세페이지로 넘어감
+        myReview1 = (LinearLayout) findViewById(R.id.my_review1);
+        myReview1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MyReviewDetailActivity.class);
+                startActivity(intent);
+            }
+        });
         // 하단바 underbar_home 버튼 등록 및 리스너 구현
         btnHome = (ImageButton)findViewById(R.id.underbar_home);
         btnHome.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/reviews/MyReviewDetailActivity.java
+++ app/src/main/java/com/example/reviews/MyReviewDetailActivity.java
@@ -10,44 +10,11 @@ import android.widget.ImageButton;
 // 마이페이지 -> 작성한코멘트 목록 -> 작성한코멘트 상세페이지 java 파일
 public class MyReviewDetailActivity extends AppCompatActivity {
 
-    // 하단바 버튼
-    ImageButton btnHome;
-    ImageButton btnSocial;
-    ImageButton btnMypage;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage_review_detail);
 
-        // 하단바 underbar_home 버튼 등록 및 리스너 구현
-        btnHome = (ImageButton)findViewById(R.id.underbar_home);
-        btnHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent); // 액티비티 띄우기
-            }
-        });
 
-        // 하단바 underbar_social 버튼 등록 및 리스너 구현
-        btnSocial = (ImageButton)findViewById(R.id.underbar_social);
-        btnSocial.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SocialActivity.class);
-                startActivity(intent); // 액티비티 띄우기
-            }
-        });
-
-        // 하단바 underbar_mypage 버튼 등록 및 리스너 구현
-        btnMypage = (ImageButton)findViewById(R.id.underbar_mypage);
-        btnMypage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MypageActivity.class);
-                startActivity(intent); // 액티비티 띄우기
-            }
-        });
     }
 }
