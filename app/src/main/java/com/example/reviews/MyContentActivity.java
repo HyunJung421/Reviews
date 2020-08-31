d66a76f7f5cd3dd6dc44bbd2b8990c4de5b37cff
Yulim1003 <skaibloo99@naver.com>
2020-08-31 21:01:20 +0900

    mypage_content java파일 생성

--- app/src/main/java/com/example/reviews/MyContentActivity.java
+++ app/src/main/java/com/example/reviews/MyContentActivity.java
@@ -1,4 +1,71 @@
 package com.example.reviews;
 
-public class MyContentActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+// 마이페이지 -> 추천한콘텐츠 목록 페이지 java 파일
+public class MyContentActivity extends AppCompatActivity {
+    // 추천한컨텐츠의 콘텐츠
+    LinearLayout myContent1;
+
+    // 영화 포스터
+    ImageView poster;
+
+    // 하단바 버튼
+    ImageButton btnHome;
+    ImageButton btnSocial;
+    ImageButton btnMypage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage_content);
+
+        // 영화 포스터 클릭시 영화정보페이지로 넘어감
+        poster = (ImageView)findViewById(R.id.movie_poster);
+        poster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MovieInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // 하단바 underbar_home 버튼 등록 및 리스너 구현
+        btnHome = (ImageButton)findViewById(R.id.underbar_home);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent); // 액티비티 띄우기
+            }
+        });
+
+        // 하단바 underbar_social 버튼 등록 및 리스너 구현
+        btnSocial = (ImageButton)findViewById(R.id.underbar_social);
+        btnSocial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SocialActivity.class);
+                startActivity(intent); // 액티비티 띄우기
+            }
+        });
+
+        // 하단바 underbar_mypage 버튼 등록 및 리스너 구현
+        btnMypage = (ImageButton)findViewById(R.id.underbar_mypage);
+        btnMypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MypageActivity.class);
+                startActivity(intent); // 액티비티 띄우기
+            }
+        });
+    }
 }
