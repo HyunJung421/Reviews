abc72da807cf3f34160ca9b3640a7a30b64d815b
eb1105 <omokok23@naver.com>
2020-08-31 17:57:19 +0900

    LoginIdShowActivity.java / LoginPwShowActivity.java 파일 생성 AndroidManifest.xml에 추가하기

---
+++ app/src/main/java/com/example/reviews/LoginIdShowActivity.java
@@ -0,0 +1,40 @@
+package com.example.reviews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//아이디 찾고 아이디 출력하는 java 파일
+public class LoginIdShowActivity extends AppCompatActivity {
+
+    Button btnlogin, btnfindpw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_id_find2);
+
+        // 로그인 하는 xml로 이동하기
+        btnlogin = (Button)findViewById(R.id.mypage_before_login_btn_login);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 비밀번호 찾는 xml로 이동하기
+        btnfindpw = (Button)findViewById(R.id.mypage_before_login_btn_findpw);
+        btnfindpw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginPwFindActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/reviews/LoginPwShowActivity.java
@@ -0,0 +1,29 @@
+package com.example.reviews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+// 비밀번호 찾고 비밀번호 출력하는 java 파일
+public class LoginPwShowActivity extends AppCompatActivity {
+
+    Button btnlogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_id_find2);
+
+        // 로그인 하는 xml로 이동하기
+        btnlogin = (Button)findViewById(R.id.mypage_before_login_btn_login);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+}
