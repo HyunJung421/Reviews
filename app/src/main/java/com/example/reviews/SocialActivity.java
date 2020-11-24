50e715a4db099df757498c97e2ced6470e4b902f
eb1105 <omokok23@naver.com>
2020-11-24 19:40:31 +0900

    소셜페이지 리뷰어 고정

--- app/src/main/java/com/example/reviews/SocialActivity.java
+++ app/src/main/java/com/example/reviews/SocialActivity.java
@@ -20,9 +20,6 @@ public class SocialActivity extends AppCompatActivity {
     TextView movieTitle2;
     TextView movieTitle3;
 
-    // 소셜인 추천수
-    TextView social_recommendation1,social_recommendation2,social_recommendation3,social_recommendation4;
-
     // 추천 영화 포스터
     ImageButton socialMovie1;
     ImageButton socialMovie2;
@@ -33,28 +30,23 @@ public class SocialActivity extends AppCompatActivity {
     ImageButton btnSocial;
     ImageButton btnMypage;
 
-    // 좋아요 여부 저장
-    boolean likeStste = false;
-    // 좋아요 증가치
-    int likeCount = 1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.social_layout);
 
         // 추천 영화 제목
-        movieTitle1 = (TextView)findViewById(R.id.movie_info_title1);
+        movieTitle1 = (TextView) findViewById(R.id.movie_info_title1);
         final String title1 = movieTitle1.getText().toString(); // 첫번째 영화 제목
 
-        movieTitle2 = (TextView)findViewById(R.id.movie_info_title2);
+        movieTitle2 = (TextView) findViewById(R.id.movie_info_title2);
         final String title2 = movieTitle2.getText().toString(); // 두번째 영화 제목
 
-        movieTitle3 = (TextView)findViewById(R.id.movie_info_title3);
+        movieTitle3 = (TextView) findViewById(R.id.movie_info_title3);
         final String title3 = movieTitle3.getText().toString(); // 세번째 영화 제목
 
         // 추천 첫번째 영화 포스터 등록 및 리스너 구현
-        socialMovie1 = (ImageButton)findViewById(R.id.social_movie1);
+        socialMovie1 = (ImageButton) findViewById(R.id.social_movie1);
         socialMovie1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,7 +56,7 @@ public class SocialActivity extends AppCompatActivity {
             }
         });
 
-        socialMovie2 = (ImageButton)findViewById(R.id.social_movie2);
+        socialMovie2 = (ImageButton) findViewById(R.id.social_movie2);
         socialMovie2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,7 +66,7 @@ public class SocialActivity extends AppCompatActivity {
             }
         });
 
-        socialMovie3 = (ImageButton)findViewById(R.id.social_movie3);
+        socialMovie3 = (ImageButton) findViewById(R.id.social_movie3);
         socialMovie3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,7 +78,7 @@ public class SocialActivity extends AppCompatActivity {
 
 
         // 하단바 underbar_home 버튼 등록 및 리스너 구현
-        btnHome = (ImageButton)findViewById(R.id.underbar_home);
+        btnHome = (ImageButton) findViewById(R.id.underbar_home);
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,7 +88,7 @@ public class SocialActivity extends AppCompatActivity {
         });
 
         // 하단바 underbar_social 버튼 등록 및 리스너 구현
-        btnSocial = (ImageButton)findViewById(R.id.underbar_social);
+        btnSocial = (ImageButton) findViewById(R.id.underbar_social);
         btnSocial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -105,7 +97,7 @@ public class SocialActivity extends AppCompatActivity {
         });
 
         // 하단바 underbar_mypage 버튼 등록 및 리스너 구현
-        btnMypage = (ImageButton)findViewById(R.id.underbar_mypage);
+        btnMypage = (ImageButton) findViewById(R.id.underbar_mypage);
         btnMypage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -113,80 +105,5 @@ public class SocialActivity extends AppCompatActivity {
                 startActivity(intent); // 액티비티 띄우기
             }
         });
-
-        // 소셜인 추천 버튼 등록 및 리스너 구현
-        social_recommendation1 = (TextView)findViewById(R.id.social_recommendation1);
-        social_recommendation1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (likeStste) {
-                    decrLikeCount(social_recommendation1);
-                } else {
-                    incrLikeCount(social_recommendation1);
-                }
-                likeStste = !likeStste;
-            }
-        });
-
-        social_recommendation2 = (TextView)findViewById(R.id.social_recommendation2);
-        social_recommendation2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (likeStste) {
-                    decrLikeCount(social_recommendation2);
-                } else {
-                    incrLikeCount(social_recommendation2);
-                }
-                likeStste = !likeStste;
-            }
-        });
-
-        social_recommendation3 = (TextView)findViewById(R.id.social_recommendation3);
-        social_recommendation3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (likeStste) {
-                    decrLikeCount(social_recommendation3);
-                } else {
-                    incrLikeCount(social_recommendation3);
-                }
-                likeStste = !likeStste;
-            }
-        });
-
-        social_recommendation4 = (TextView)findViewById(R.id.social_recommendation4);
-        social_recommendation4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (likeStste) {
-                    decrLikeCount(social_recommendation4);
-                } else {
-                    incrLikeCount(social_recommendation4);
-                }
-                likeStste = !likeStste;
-            }
-        });
-    }
-
-    // 좋아요 +1 메소드
-    private void incrLikeCount(TextView recomm) {
-        likeCount++;
-        // 파란따봉으로 이미지 변경
-        Drawable img = getResources().getDrawable(R.drawable.drawable_left_image_customise);
-        img.setBounds(0,0,60,60);
-        recomm.setCompoundDrawables(img, null, null, null);
-        // 좋아요 숫자 파란색으로 변경
-        recomm.setTextColor(Color.parseColor("#0000E1"));
-    }
-
-    // 좋아요 -1 메소드
-    public void decrLikeCount(TextView recomm) {
-        likeCount--;
-        // 회색따봉으로 이미지 변경
-        Drawable img = getResources().getDrawable(R.drawable.unlike_left_image_customise);
-        img.setBounds(0,0,60,60);
-        recomm.setCompoundDrawables(img, null, null, null);
-        // 좋아요 숫자 회색으로 변경
-        recomm.setTextColor(Color.parseColor("#999999"));
     }
 }
